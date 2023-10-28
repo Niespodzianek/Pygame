71c5139a92041cc9b788f410f2867601c75507dc
Niespodzianek <michal_grzyb@yahoo.pl>
2023-10-28 02:37:15 +0200

    Przygotowanie pliku startowego

--- plik_startowy_pygame.py
+++ plik_startowy_pygame.py
@@ -6,27 +6,19 @@ PLANSZA_GRY = pygame.display.set_mode((SZEROKOSC_PLANSZY, WYSOKOSC_PLANSZY))
 pygame.display.set_caption("Gra")
 FPS = 60
 
-def drawing(gracz):
+def drawing():
     PLANSZA_GRY.fill("red")
-    pygame.draw.rect(PLANSZA_GRY, "white", (gracz.x, gracz.y, gracz.width, gracz.height))
     pygame.display.update()
 
 def main_loop():
     game_speed = pygame.time.Clock()
     game_speed.tick(FPS)
-
-    gracz = pygame.Rect(500, 500, 200, 200)
-    print(type(gracz))
-    print(dir(gracz))
-    input("Naciśnij ENTER")
-
     program_pracuje = True
     while program_pracuje:
         for zdarzenie in pygame.event.get():
             if zdarzenie.type == pygame.QUIT:
                 program_pracuje = False
-
-        drawing(gracz)
+        drawing()
     pygame.quit()
 
 def program():
